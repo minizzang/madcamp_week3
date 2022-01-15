d0b8708d1468a2e146621f54fb82b00e5bf594cd
pell13 <peixueying@gmail.com>
2022-01-15 17:18:11 +0900

    Fix : Show year of cards, change photo on Storage.js

--- react-app/src/components/Storage.js
+++ react-app/src/components/Storage.js
@@ -5,21 +5,31 @@ const Storage = () => {
 
   return (
     <>
-<div class="container_grid">
-  <div class="box">
-  몇년이게 
-  </div>
-        <div class="box"> {/* 나중에 map 함수 써서 무한생산하면 될 듯. */}
-            <div class="imgBox">
-                <img src="https://img.freepik.com/free-photo/portrait-handsome-young-man-makes-okay-gesture-demonstrates-agreement-likes-idea-smiles-happily-wears-optical-glasses-yellow-hat-t-shirt-models-indoor-its-fine-thank-you-hand-sign_273609-30676.jpg?size=626&ext=jpg" alt=""/>
-                <h2>7월</h2>
+    <div class = "letter_box">
+    <div class = "storage_year">2020년의 추억들</div>
+    <div class="container_grid">
+            <div class="box"> {/* 나중에 map 함수 써서 무한생산하면 될 듯. */}
+                <div class="imgBox">
+                    <img src="https://img.freepik.com/free-photo/top-view-envelope-flower-book_23-2148213913.jpg?size=626&ext=jpg" alt=""/>
+                    <h2>1월</h2>
+                </div>
+                <div class="content">
+                    <span>72장 의 카드가 있어요!</span>
+                </div>
             </div>
-            <div class="content">
-                <span>72장 의 카드가 있어요!</span>
+
+            <div class="box"> {/* 나중에 map 함수 써서 무한생산하면 될 듯. */}
+                <div class="imgBox">
+                    <img src="https://img.freepik.com/free-photo/top-view-envelope-flower-book_23-2148213913.jpg?size=626&ext=jpg" alt=""/>
+                    <h2>7월</h2>
+                </div>
+                <div class="content">
+                    <span>72장 의 카드가 있어요!</span>
+                </div>
             </div>
-        </div>
-</div>
-<span>72장 의 카드가 있어요!</span>
+    </div>
+    </div>
+
 </>
   );
 }
