07d7ff9ba8f39c4b58b389209d1510e3204ebebb
pell13 <peixueying@gmail.com>
2022-01-16 20:52:12 +0900

    Fix : Select only one button in WriteLetter.js

--- react-app/src/components/WriteLetter.js
+++ react-app/src/components/WriteLetter.js
@@ -13,7 +13,7 @@ const Write = () => {
     const [contents, setContents] = useState("");
     const [subject, setSubject] = useState("");
     const [paperType, setpaperType] = useState("");
-    const [btnSelected, setbtnSelected] = useState("AAAAAA");
+    const [effectType, setEffectType] = useState("");
     const [error, setError] = useState("");
     const [startDate, setStartDate] = useState(new Date());
 
@@ -72,29 +72,40 @@ const Write = () => {
     //시간을 년-월-일 형식으로 변환해주는 함수
 
     // 체크됐는지 확인해서 애니메이션 진행
-  const onSelect = (event) => {
+  const onSelectPaper = (event) => {
+
+    document.querySelectorAll(`div[type=paper]`).forEach(el => el.className = "btn"); //모든걸 버튼으로 바꿔주기!
+
+    event.target.className = "btn_selected" //내가 선택한 건 selected로 바꿔주기!
     const {
       target: { name },
       } = event;
         setpaperType(name);
-        setColor('#CCCCCC');
-
-        this.setState({
-          text: "변경 성공!",
-        });
   };
+
+  const onSelecEffect = (event) => {
+
+    document.querySelectorAll(`div[type=effect]`).forEach(el => el.className = "btn"); //모든걸 버튼으로 바꿔주기!
+
+    event.target.className = "btn_selected" //내가 선택한 건 selected로 바꿔주기!
+    const {
+      target: { name },
+      } = event;
+        setEffectType(name);
+  };
+
+  
 
   return (
       <>
-
       <div class = "write_letter_box">
 
       <div class = "custom_tab">
         <h3>편지지를 골라요!</h3>
-        <button className="btn" className ={btnSelected} name="paper1" onClick={onSelect} >편지지 1</button>
-        <button className="btn" name="paper2" onClick={onSelect}>편지지 2</button>
-        <button className="btn" name="paper3" onClick={onSelect}>편지지 3</button>
-        <button className="btn" name="paper4" onClick={onSelect}>편지지 4</button>
+        <div className="btn" type= "paper" name="paper1" onClick={ onSelectPaper}>편지지 1</div>
+        <div className="btn" type= "paper" name="paper2" onClick={ onSelectPaper}>편지지 2</div>
+        <div className="btn" type= "paper" name="paper3" onClick={ onSelectPaper}>편지지 3</div>
+        <div className="btn" type= "paper" name="paper4" onClick={ onSelectPaper}>편지지 4</div>
       </div>
 
       <div class = "Blank"></div>
@@ -142,13 +153,16 @@ const Write = () => {
         className="writeInput writeSubmit"
         value={"보내기"}/>
         </form>
-
         </div>
 
         <div class = "Blank"></div>
 
         <div class = "custom_tab">
           <h3>효과를 골라요!</h3>
+          <div className="btn" type= "effect" name="effect1" onClick={onSelecEffect}>효과 1</div>
+          <div className="btn" type= "effect" name="effect2" onClick={onSelecEffect}>효과 2</div>
+          <div className="btn" type= "effect" name="effect3" onClick={onSelecEffect}>효과 3</div>
+          <div className="btn" type= "effect" name="effect4" onClick={onSelecEffect}>효과 4</div>
           </div>
 
         </div>
